460308d3edbf6f907287bb34716d5881973e7c38
Daliso Ngoma <daliso@djngoma.com>
2026-03-31 22:41:37 +0200

    Install the splash handoff and dismiss the phantom top bar

--- android/app/src/main/java/com/djngoma/devilsaidictionary/DictionaryActivity.kt
+++ android/app/src/main/java/com/djngoma/devilsaidictionary/DictionaryActivity.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class DictionaryActivity : ComponentActivity() {
     private lateinit var store: NativeDictionaryStore
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
